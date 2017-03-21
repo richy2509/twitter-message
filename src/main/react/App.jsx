6ad6738b560045ca236480774bc76258ae7797a0
richard koehl <koehl.richard@gmail.com>
2017-03-21 22:42:32 +0100

    add bootstrap

--- src/main/react/App.jsx
+++ src/main/react/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import TitleBar from "./title/TitleBar";
 import HeaderBar from "./header/HeaderBar";
 import Tweet from "./tweet/Tweet";
+import {Col, Grid, Row} from "react-bootstrap";
 
 class App extends Component {
 
@@ -12,11 +13,33 @@ class App extends Component {
     }
 
     render() {
+        {/*<Grid>
+         <Row className="show-grid">
+         <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+         <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+         </Row>
+
+         <Row className="show-grid">
+         <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+         <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+         <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+         </Row>
+
+         <Row className="show-grid">
+         <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+         </Row>
+
+         <Row className="show-grid">
+         <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+         <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+         </Row>
+         </Grid>*/}
         return (
+
             <div className="App">
-                <HeaderBar />
-                <TitleBar/>
-                <Tweet/>
+            <HeaderBar />
+            <TitleBar/>
+            <Tweet/>
             </div>
         );
     }
